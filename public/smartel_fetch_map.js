2f21717d32afb135bcc69da5950f7bc4213b22b7
usimmarket <kukidream@gmail.com>
2025-10-15 18:07:04 +0900

    Update smartel_fetch_map.js

--- public/smartel_fetch_map.js
+++ public/smartel_fetch_map.js
@@ -1,20 +1,48 @@
+<script>
 (function(){
   'use strict';
-  if (window.__SMARTEL_FETCH_MAP__) return; window.__SMARTEL_FETCH_MAP__ = true;
-  var REL = '/.netlify/functions/generate_pdf';
-  var ABS = 'https://smartel.netlify.app/.netlify/functions/generate_pdf';
+  if (window.__SMARTEL_TRANSPORT_MAP__) return; window.__SMARTEL_TRANSPORT_MAP__ = true;
+
+  var ABS_BASE   = 'https://smartel.netlify.app'; // Netlify 사이트 도메인
+  var REL_PREFIX = '/.netlify/functions/';        // 함수 경로 접두사 (배포와 다르면 여기만 바꿔주세요)
+
   function onNetlifyHost(){
-    try{ return /smartel\.netlify\.app$/i.test(location.hostname); }catch(_){ return false; }
+    try { return /smartel\.netlify\.app$/i.test(location.hostname); } catch(_) { return false; }
   }
+  function normalizeToAbs(input){
+    try{
+      var u = new URL(input, location.href); // './', '../' 정규화
+      if (!onNetlifyHost() && u.pathname.startsWith(REL_PREFIX)){
+        return ABS_BASE + u.pathname + (u.search || '');
+      }
+      return input;
+    }catch(_){ return input; }
+  }
+
+  // fetch 재작성
   var _fetch = window.fetch;
   window.fetch = function(input, init){
     try{
-      var url = (typeof input === 'string') ? input : (input && input.url);
-      if (!onNetlifyHost() && url === REL){
-        input = ABS;
+      if (typeof input === 'string'){
+        input = normalizeToAbs(input);
+      } else if (input && typeof input.url === 'string'){
+        var abs = normalizeToAbs(input.url);
+        if (abs !== input.url) input = new Request(abs, input); // Request 복제
       }
-    }catch(_){}
-    return _fetch.apply(this, arguments);
+    }catch(e){ console.warn('[transport-map] fetch rewrite skipped:', e); }
+    return _fetch.call(this, input, init);
   };
-  console.log('[fetch-map] Relative function URL rewritten when off Netlify host');
+
+  // XHR 재작성
+  var _open = XMLHttpRequest.prototype.open;
+  XMLHttpRequest.prototype.open = function(method, url){
+    try{ if (typeof url === 'string') url = normalizeToAbs(url); }catch(_) {}
+    // 인자 재구성해서 원본 open 호출
+    var args = Array.prototype.slice.call(arguments);
+    args[1] = url;
+    return _open.apply(this, args);
+  };
+
+  console.log('[transport-map] fetch + XHR URL rewrite active (off Netlify host)');
 })();
+</script>
